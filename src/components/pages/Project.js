3c1286574edc5db3b43fa7e218b8c94a47570532
Jun Ming <junming518@gmail.com>
2020-03-26 16:36:36 +0800

    add few data at project task

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,19 +3,10 @@ import React, { Component } from 'react'
 export class Project extends Component {  
   render() {
 
-    // this.props.data.map((data) => (
-
-    //   console.log(data)
-    //  ));
-
-    // return this.props.data.map((task) =>(
-    //   console.log(task)
-    // ));
-
 
     return(
       <div  className="container-about">
-        <h1 id="project">Projects</h1>
+        <h1 id="project">Projects Task</h1>
         {this.props.data.map(data => (
           
           <li key={data.id} >{  data.name  }</li>
